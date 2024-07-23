35c5e6964570aa5985756367f6384d01e14c6b3f
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-07-23 15:47:17 +0100

    moved the Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
-package org.example;
+package org.example.vehicles;
 
 public class Main {
+
     public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Prado", false, 160, "Diesel" );
+        myCar.start();
+        myCar.drive();
+        myCar.accelerate();
 
-
-
+        Motorcycle myMotorcycle = new Motorcycle("Suzuki", "SomeModel", false, 220, "Petrol", false);
+        myMotorcycle.start();
+        myMotorcycle.drive();
+        myMotorcycle.accelerate();
     }
 
 }
